5b6954e9783d742ee88e60c22dfad43c44bd44ee
Alex <alex@kovalskitech.com>
2024-03-09 11:13:35 -0500

    Another one

--- app/example/index.tsx
+++ app/example/index.tsx
@@ -3,14 +3,9 @@ import BUHHS from "./media/BUHHS-Logo.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <Image src={BUHHS} alt="BUHHS Logo" width={250} height={250}/> 
-      </div>
-      <div>
-        <h1>
-          <p>Hello, this is my 2024 Boston University Hacks High School BUHHS</p>
-        </h1>
       </div>
     </main>
   );
