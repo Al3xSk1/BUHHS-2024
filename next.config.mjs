95d93b7bf68bb8f4f169ac09d403eaeecc6f79e4
Alex <alex@kovalskitech.com>
2024-03-09 11:29:12 -0500

    Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/BUHHS-2024",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
